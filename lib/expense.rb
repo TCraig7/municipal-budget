a43214ba60a9d2eb38e36f2bf6e928f6896b453e
Tara Craig <tara.craig7@gmail.com>
2018-06-21 14:59:39 -0600

    Add expense class and tests

---
+++ lib/expense.rb
@@ -0,0 +1,13 @@
+class Expense
+
+  attr_reader   :department,
+                :budget_category,
+                :amount
+
+  def initialize(department, budget_category, amount)
+    @department = department
+    @budget_category = budget_category
+    @amount = "%.2f" % amount
+  end
+
+end
